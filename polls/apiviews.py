838217aa3c6a5c4506c6c7e6a1d95bbc8edf0825
badr kamel <badrk.shokr@gmail.com>
2019-04-29 02:18:53 +0300

    Change API views to generics views

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,20 +1,16 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from .models import Poll, Choice
+from .serializers import (
+		PollSerializer, 
+		ChoiceSerializer,
+		VoteSerializer
+	)
 
-from .models import Poll
-from .serializers import (PollSerializer)
 
-class PollList(APIView):
-	def get(self, request):
-		MAX_OBJECTS = 20
-		polls = Poll.objects.all()[:MAX_OBJECTS]
-		data = PollSerializer(polls, many=True).data
-		return Response(data)
+class PollList(generics.ListCreateAPIView):
+	queryset = Poll.objects.all()
+	serializer_class = PollSerializer
 
-class PollDetail(APIView):
-	def get(self, request, pk):
-		poll = get_object_or_404(Poll, pk=pk)
-		data = PollSerializer(poll).data
-		return Response(data)
-		
+class PollDetail(generics.RetrieveDestroyAPIView):
+	queryset = Poll.objects.all()
+	serializer_class = PollSerializer
